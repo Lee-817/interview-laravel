09e1cf702004c6f0d0057a3a6486de1137993a0e
Fischer-Ouji <150597619+Lee-817@users.noreply.github.com>
2023-11-16 17:32:39 +0800

    enhance GET /students api
    -include courses associated to student

--- src/app/Http/Controllers/Api/StudentController.php
+++ src/app/Http/Controllers/Api/StudentController.php
@@ -5,13 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Student;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     public function index(): JsonResponse
     {
         try {
-            $data = Student::all();
+            $query = Student::with('courses');
+            $email = request('email');
+
+            if ($email) {
+                $query->whereEmail($email);
+            }
+
+            $data = $query->get();
 
             return response()->json([
                 'status' => true,
