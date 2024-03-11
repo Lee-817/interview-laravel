30d2c322e1d3a42d348de7d29a01eafab1dadfed
l.szeyu@toyeight.com <l.szeyu@toyeight.com>
2024-03-11 21:18:49 +0800

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzOTU4Nw/spectral.js?branch_slug=main&token=2a7572f3-c297-4e0a-98fb-20c8ce010bf3";
+export default { extends: ruleset };
